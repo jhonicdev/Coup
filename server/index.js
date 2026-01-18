14cd3faa9d48c9cda17ecad0e84c522a8394aabc
jhonicdev <jonatasnicolau4@gmail.com>
2026-01-18 16:07:52 -0300

    Backend inicial

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend do Coup está rodando!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
